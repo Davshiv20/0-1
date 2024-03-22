48eb1979c3ee72077cb6d97ea687d79d2f8624c2
Shivam <shivamdave2903@gmail.com>
2024-03-23 00:08:59 +0530

    easy_async

--- assignments-master/week-2/01-async-js/easy/read.js
+++ assignments-master/week-2/01-async-js/easy/read.js
@@ -1,5 +1,9 @@
 const fs=require('fs').promises;
 async function reading(){
+
+    const data2=" hi shivam hi hi hih ih  shd";
+    
+    fs.appendFile("a.txt",data2);
     const data=await fs.readFile("a.txt",'utf-8')
     console.log(data);
 }
